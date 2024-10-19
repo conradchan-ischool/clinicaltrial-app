295f6dff152c3381c7e333095193d4692ffd030f
Conrad Chan <conradchan415@berkeley.com>
2024-10-19 10:10:35 -0700

    Added sample json for testing, create sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,41 @@
 import streamlit as st
+import json
+
+def call_retrieve_trials_API(patient):
+    """
+    Call the API to get matching clinical trials.
+    For now, this will read a json file of clinical trials.
+    """
+    clinical_trials_file = open("clinical_trials_sample.json")
+
+    return json.load(clinical_trials_file)
+
 
 def find_trials(patient):
     """Function to find clinical trials."""
     st.write("Searching for clinical trials for", patient["condition"]," ...")
+    return call_retrieve_trials_API(patient)
+    
     
 def main():
     st.title("Clinical Trials App")
-    st.write(
+    st.sidebar.write(
         "Find a clinical trial that's right for you."
     )
-    st.write(
+    st.sidebar.write(
         "Enter your information below."
     )
 
-    patient_form = st.form(key="patient")
+    # patient_form = st.form(key="patient")
     
-    age = patient_form.number_input("Age", 0, 100, 0, 1)
-    sex = patient_form.radio("Sex", ["Female", "Male"])
-    condition = patient_form.text_input("Medical Condition")
-    acceptsHealthy = patient_form.checkbox("Accepts healthy volunteers", value=False)
-    location = patient_form.text_input("Location")
-    distance = patient_form.number_input("Miles you are able to travel")
+    age = st.sidebar.number_input("Age", 0, 100, 0, 1)
+    sex = st.sidebar.radio("Sex", ["Female", "Male"])
+    condition = st.sidebar.text_input("Medical Condition")
+    acceptsHealthy = st.sidebar.checkbox("Accepts healthy volunteers", value=False)
+    location = st.sidebar.text_input("Location")
+    distance = st.sidebar.number_input("Miles you are able to travel", 0, 10000, 0, 1)
 
-    submit_button = patient_form.form_submit_button(label="Find clinical trials")
+    submit_button = st.sidebar.button(label="Find clinical trials")
     if submit_button:
         # Create a dictionary for the patient profile
         patient = {}
@@ -34,6 +47,8 @@ def main():
         patient["distance"] = distance
         
         find_trials(patient)
+        
+        # Display results
         
         
     
