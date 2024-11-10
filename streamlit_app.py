6d870a4a054843d86326a544c1558de07eb44c01
Conrad Chan <conradchan415@berkeley.com>
2024-11-09 17:53:27 -0800

    Redo chatbot code.  Got rid of sidebar for input.

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,48 +125,101 @@ def show_clinical_trials(json_list):
     
 def main():
     
-    load_conditions() # Load list of medical conditions user can search from
-    
-    st.title("AllTrials.info")
-    st.header("Matching Clinical Trials")
-    
-    # Enter search criteria in the sidebar
-    with st.sidebar:
-        st.title("Search for Clinical Trials")
-        st.write(
-                    "Enter your information below."
-                )   
-        age = st.number_input("Age", 0, 100, 0, 1)
-        sex = st.radio("Sex", ["Female", "Male"])
-        # condition = st.text_input("Medical Condition")
-        condition = st_searchbox(search_function=find_conditions,
-                                 label="Medical Condition",
-                                 key="condition_searchbox")
-        conditionText = st.text_area("Additional Information")
-        acceptsHealthy = st.checkbox("Accepts healthy volunteers", value=False)
-        location = st.text_input("Location")
-        distance = st.number_input("Miles you are able to travel", 0, 10000, 0, 1)
+    load_conditions()  # Load list of medical conditions user can search from
 
-        submit_button = st.button(label="Find clinical trials")
-    
-    if submit_button:
-        # Create a dictionary for the patient profile
-        patient = {}
-        patient["condition"] = condition
-        patient["conditionText"] = conditionText
-        patient["age"] = age
-        patient["sex"] = sex
-        patient["acceptsHealthy"] = acceptsHealthy
-        patient["location"] = location
-        patient["distance"] = distance
+    # Show title and description.
+    st.title("AllTrials.info")
+    st.write(
+        "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+        "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
+        "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    )
+
+    # Ask user for their OpenAI API key via `st.text_input`.
+    # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
+    # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
+    openai_api_key = st.text_input("OpenAI API Key", type="password")
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    else:
+
+        # Create an OpenAI client.
+        client = OpenAI(api_key=openai_api_key)
+
+        # Create a session state variable to store the chat messages. This ensures that the
+        # messages persist across reruns.
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
         
-        ct_results = find_trials(patient)
+        if "ct_results" not in st.session_state:
+            st.session_state.ct_results = []
         
-        # Display results
-        st.write("We found ", len(ct_results), "clinical trials you might qualify for.")
-        show_clinical_trials(ct_results)
-                        
         
+        with st.form("input"):
+            st.title("Search for Clinical Trials")
+            st.write("Enter your information below.")   
+            age = st.number_input("Age", 0, 100, 0, 1)
+            sex = st.radio("Sex", ["Female", "Male"])
+            # condition = st.text_input("Medical Condition")
+            condition = st_searchbox(search_function=find_conditions,
+                                        label="Medical Condition",
+                                        key="condition_searchbox")
+            conditionText = st.text_area("Additional Information")
+            acceptsHealthy = st.checkbox("Accepts healthy volunteers", value=False)
+            location = st.text_input("Location")
+            distance = st.number_input("Miles you are able to travel", 0, 10000, 0, 1)
+
+            submit_button = st.form_submit_button(label="Find clinical trials")
+            
+            if submit_button:
+                # Create a dictionary for the patient profile
+                patient = {}
+                patient["condition"] = condition
+                patient["conditionText"] = conditionText
+                patient["age"] = age
+                patient["sex"] = sex
+                patient["acceptsHealthy"] = acceptsHealthy
+                patient["location"] = location
+                patient["distance"] = distance
+                
+                st.session_state.ct_results = find_trials(patient)
+                
+            # Display results
+            if len(st.session_state.ct_results):
+                st.write("We found ", len(st.session_state.ct_results), "clinical trials you might qualify for.")
+                show_clinical_trials(st.session_state.ct_results)
+                
+
+        # Display the existing chat messages via `st.chat_message`.
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+            
+        # Create a chat input field to allow the user to enter a message. This will display
+        # automatically at the bottom of the page.
+        if prompt := st.chat_input("What is up?"):
+
+            # Store and display the current prompt.
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+
+            # Generate a response using the OpenAI API.
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            with st.chat_message("assistant"):
+                response = st.write_stream(stream)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            
     
 if __name__ == "__main__":
     main()
